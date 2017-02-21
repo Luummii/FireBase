66b9510dcf897ade53fbe0291f0266a336593802
QtMobile <zimin6077@gmail.com>
2017-02-21 21:09:20 +0300

    Add files via upload

---
+++ FireBase/firebase.cpp
@@ -0,0 +1,155 @@
+#include "firebase.h"
+#include <QDir>
+
+FireBase::FireBase(QObject *parent) : QObject(parent)
+{
+
+}
+
+FireBase::FireBase(QString &http, const QString &token)
+{
+    urlFireBase = http;
+    this->token = token;    
+}
+
+FireBase::FireBase(QJsonObject &object) :document(object)
+{
+
+}
+
+FireBase *FireBase::child(QString &childName)
+{
+    HTTP.clear();
+    HTTP.append(urlFireBase).append(childName).append(".json?auth=").append(token);    
+    FireBase *firebase = new FireBase();
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);
+    return firebase;
+}
+
+FireBase *FireBase::value(QJsonObject &object)
+{
+    FireBase *firebase = new FireBase(object);
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);    
+    return firebase;
+}
+
+void FireBase::push(){ connectFireBase(QByteArray("POST"));}
+
+void FireBase::set(){ connectFireBase(QByteArray("PUT"));}
+
+void FireBase::update(){ connectFireBase(QByteArray("PATCH"));}
+
+void FireBase::remove(){ connectFireBase(QByteArray("DELETE"));}
+
+QString FireBase::get()
+{
+    QUrl url = QUrl::fromUserInput(HTTP);    
+    QNetworkRequest request;    
+    request.setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
+    request.setRawHeader("Accept-Encoding","identity");
+    request.setUrl(url);
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    manager->get(request);
+
+    QEventLoop wait;
+    connect(manager, &QNetworkAccessManager::finished,
+            this, &FireBase::networkRequestResult);
+    connect(manager, &QNetworkAccessManager::finished,
+            manager, &QNetworkAccessManager::deleteLater);
+    connect(manager, &QNetworkAccessManager::finished,
+            this, &FireBase::destroyFireSlot);
+    connect(manager, &QNetworkAccessManager::finished,
+            &wait, &QEventLoop::quit);
+    QTimer::singleShot(10000, &wait, &QEventLoop::quit);
+    wait.exec();
+
+    return result;
+}
+
+void FireBase::connectFireBase(QByteArray method)
+{
+    QNetworkRequest request;
+    QUrl url(HTTP);    
+    request.setUrl(url);
+
+    QBuffer *buffer = new QBuffer();
+    buffer->open((QBuffer::ReadWrite));
+    buffer->write(document.toJson());
+    buffer->seek(0);
+    buffer->close();
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    manager->sendCustomRequest(request, method, buffer);
+
+    connect(manager, &QNetworkAccessManager::finished,
+            buffer, &QBuffer::deleteLater);
+    connect(manager, &QNetworkAccessManager::finished,
+            manager, &QNetworkAccessManager::deleteLater);
+    connect(manager, &QNetworkAccessManager::finished,
+            this, &FireBase::destroyFireSlot);
+}
+
+FireBase *FireBase::orderBy(QString &orderBy)
+{
+    QString str = QString("&orderBy=%22%1%22").arg(orderBy);
+    HTTP.append(str);
+    FireBase *firebase = new FireBase();
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);
+    return firebase;
+}
+
+FireBase *FireBase::startAt(int start_at)
+{
+    QString str = QString("&startAt=%1").arg(start_at);
+    HTTP.append(str);
+    FireBase *firebase = new FireBase();
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);
+    return firebase;
+}
+
+FireBase *FireBase::limitToFirst(int limit_to_first)
+{    
+    QString str = QString("&limitToFirst=%1").arg(limit_to_first);
+    HTTP.append(str);
+    FireBase *firebase = new FireBase();
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);
+    return firebase;
+}
+
+FireBase *FireBase::endAt(int end_at)
+{
+    QString str = QString("&endAt=%1").arg(end_at);
+    HTTP.append(str);
+    FireBase *firebase = new FireBase();
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);
+    return firebase;
+}
+
+FireBase *FireBase::limitToLast(int limit_to_last)
+{
+    QString str = QString("&limitToLast=%1").arg(limit_to_last);
+    HTTP.append(str);
+    FireBase *firebase = new FireBase();
+    connect(this, &FireBase::destroyFire,
+            firebase, &FireBase::deleteLater);
+    return firebase;
+}
+
+void FireBase::networkRequestResult(QNetworkReply *reply)
+{
+    QString result = (QString)reply->readAll();
+    this->result = result;
+    reply->deleteLater();
+}
+
+void FireBase::destroyFireSlot()
+{
+    emit destroyFire();
+}
---
+++ FireBase/firebase.h
@@ -0,0 +1,89 @@
+#ifndef FIREBASE_H
+#define FIREBASE_H
+
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QEventLoop>
+#include <QTimer>
+#include <QBuffer>
+#include <QDebug>
+#include <QUrl>
+
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonValue>
+
+static QString HTTP = "";
+
+class FireBase : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit FireBase(QObject *parent = 0);
+    explicit FireBase(QString &http, const QString &token);
+    explicit FireBase(QJsonObject &object);
+
+public:
+    void push();
+    void set();
+    void update();
+    void remove();
+    QString get();
+
+public:
+    FireBase *child(QString &childName);
+    FireBase *value(QJsonObject &object);
+
+
+    FireBase *orderBy(QString &orderBy);
+    FireBase *startAt(int start_at);
+    FireBase *limitToFirst(int limit_to_first);
+    FireBase *endAt(int end_at);
+    FireBase *limitToLast(int limit_to_last);
+
+private:
+    QString urlFireBase = "";
+    QString token = "";
+    QString result = "";
+    QJsonDocument document;
+
+signals:
+    void destroyFire();
+    void sendData(QJsonObject &root);
+
+
+private slots:
+    void connectFireBase(QByteArray method);
+    void networkRequestResult(QNetworkReply *reply);
+    void destroyFireSlot();
+
+
+
+
+
+
+
+
+
+
+
+
+// public:
+//    void startToFirst(QString parent, QString order, int startAt, int limitToFirst);
+
+//private:
+//    void __PUSH(const QByteArray method, QUrl url, QJsonDocument document);
+//    void _PUSH(QUrl url);
+
+//private slots:
+//    void onResult(QNetworkReply *reply);
+
+};
+
+
+#endif // FIREBASE_H
